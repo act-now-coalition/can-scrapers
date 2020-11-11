65e074b36894dcdea2372f5d1fb49efba3478bc9
Spencer Lyon <spencerlyon2@gmail.com>
2020-11-11 15:52:28 -0500

    Add massechusets scraper

--- can_tools/scrapers/__init__.py
+++ can_tools/scrapers/__init__.py
@@ -1,12 +1,6 @@
-from .base import CMU, DatasetBaseNeedsDate, DatasetBaseNoDate, InsertWithTempTable
-
+from .base import (CMU, DatasetBaseNeedsDate, DatasetBaseNoDate,
+                   InsertWithTempTable)
 from .db_util import TempTable, fast_to_sql
-from .official import (
-    CACounty,
-    FLCounty,
-    FloridaHospital,
-    NYCounty,
-    TXCounty,
-    IllinoisDemographics,
-    IllinoisHistorical,
-)
+from .official import (CACounty, FLCounty, FloridaHospital,
+                       IllinoisDemographics, IllinoisHistorical, Massachusetts,
+                       NYCounty, TXCounty)
--- can_tools/scrapers/official/__init__.py
+++ can_tools/scrapers/official/__init__.py
@@ -1,5 +1,6 @@
 from .CA import CACounty
 from .FL import FLCounty, FloridaHospital
+from .IL import IllinoisDemographics, IllinoisHistorical
+from .MA import Massachusetts
 from .NY import NYCounty
 from .TX import TXCounty
-from .IL import IllinoisHistorical, IllinoisDemographics
