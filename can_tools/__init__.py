40ca37ea94c75cd44d48e79d073abd8425e27545
Spencer Lyon <spencerlyon2@gmail.com>
2020-11-11 11:28:51 -0500

    Populate meta.locations table

--- can_tools/__init__.py
+++ can_tools/__init__.py
@@ -23,3 +23,4 @@
 """
 
 __version__ = "0.0.1"
+from . import scrapers
---
+++ db/bin/populate_locations.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from can_tools.scrapers.uscensus.geo import USGeoBaseAPI
+
+d = USGeoBaseAPI("state")
+df_s = d.get()
+df_s["location_type"] = 2
+
+d = USGeoBaseAPI("county")
+df_c = d.get()
+df_c["location_type"] = 1
+
+df = pd.DataFrame(pd.concat([df_s, df_c], ignore_index=True))
+df["fullname"] = df["fullname"].str.replace("'", "''")
+df["name"] = df["name"].str.replace("'", "''")
+
+fmt = "({location}, {location_type}, '{state}', {area}, {latitude}, {longitude}, '{fullname}', '{name}')"
+
+inserts = ", ".join(fmt.format(**x) for _, x in df.iterrows())
+
+output = (
+    """
+INSERT INTO meta.locations (location, location_type, state, area, latitude, longitude, fullname, name)
+VALUES
+"""
+    + " "
+    + inserts
+    + ";"
+)
+
+print(output)
