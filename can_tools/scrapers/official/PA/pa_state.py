efbad63e6748734d269cc3e9b5c16ea06baddb34
Christopher Turner <turnerc@usc.edu>
2020-11-24 15:23:32 -0800

    Progress on PA scraper's normalize()
    Need to confirm assignments for metadata variables and review functionality of line beginning in "out.loc"

--- can_tools/scrapers/official/PA/pa_state.py
+++ can_tools/scrapers/official/PA/pa_state.py
@@ -18,61 +18,55 @@ class Pennsylvania(ArcGIS, DatasetBase):
     def fetch(self):
         return self.get_all_jsons("COVID_PA_Counties", 0, 1)
 
-    # def normalize(self, data):
-    #     df = self.arcgis_jsons_to_df(data)
-    #     df.columns = [x.lower() for x in list(df)]
-    #     df["location"] = (self.state_fips * 1000) + df["county"].astype(int)
+    def normalize(self, data):
+        df = self.arcgis_jsons_to_df(data)
+        df.columns = [x.lower() for x in list(df)]
 
-    #     # 12025 is the OLD (retired in 1997) fips code for Date county. It is now known
-    #     # as Miami-Dade county with fips code 12086
-    #     df.loc[:, "location"] = df["location"].replace(12025, 12086)
+        crename = {
+            "cases": CMU(category="cases", measurement="cumulative", unit="people"),
+            "deaths": CMU(category="deaths", measurement="cumulative", unit="people"),
+            "probable": CMU(
+                category="cases_probable",
+                measurement="cumulative",
+                unit="people",
+            ),
+            "negative": CMU(
+                category="unspecified_tests_negative",
+                measurement="cumulative",
+                unit="test_encounters",
+            ),
+            "confirmed": CMU(
+                category="unspecified_tests_positive",
+                measurement="cumulative",
+                unit="test_encounters",
+            ),
+        }
+        out = (
+            df.melt(id_vars=["county"], value_vars=crename.keys())
+            .assign(
+                dt=self._retrieve_dt("US/Eastern"), vintage=self._retrieve_vintage()
+            )
+            .dropna()
+        )
+        out.loc[:, "value"] = pd.to_numeric(out["value"])
 
-    #     crename = {
-    #         "casesall": CMU(category="cases", measurement="cumulative", unit="people"),
-    #         "deaths": CMU(category="deaths", measurement="cumulative", unit="people"),
-    #         "newpos": CMU(
-    #             category="unspecified_tests_positive",
-    #             measurement="new",
-    #             unit="test_encounters",
-    #         ),
-    #         "newneg": CMU(
-    #             category="unspecified_tests_negative",
-    #             measurement="new",
-    #             unit="test_encounters",
-    #         ),
-    #         "newtested": CMU(
-    #             category="unspecified_tests_total",
-    #             measurement="new",
-    #             unit="test_encounters",
-    #         ),
-    #     }
-    #     out = (
-    #         df.melt(id_vars=["location"], value_vars=crename.keys())
-    #         .assign(
-    #             dt=self._retrieve_dt("US/Eastern"), vintage=self._retrieve_vintage()
-    #         )
-    #         .query("location not in (12998, 12999)")
-    #         .dropna()
-    #     )
-    #     out.loc[:, "value"] = pd.to_numeric(out["value"])
+        # Extract category information and add other variable context
+        out = self.extract_CMU(out, crename)
 
-    #     # Extract category information and add other variable context
-    #     out = self.extract_CMU(out, crename)
+        cols_to_keep = [
+            "vintage",
+            "dt",
+            "location",
+            "category",
+            "measurement",
+            "unit",
+            "age",
+            "race",
+            "sex",
+            "value",
+        ]
 
-    #     cols_to_keep = [
-    #         "vintage",
-    #         "dt",
-    #         "location",
-    #         "category",
-    #         "measurement",
-    #         "unit",
-    #         "age",
-    #         "race",
-    #         "sex",
-    #         "value",
-    #     ]
+        return out.loc[:, cols_to_keep]
 
-    #     return out.loc[:, cols_to_keep]
-
-    # def validate(self, df, df_hist):
-    #     return True
+    def validate(self, df, df_hist):
+        return True
