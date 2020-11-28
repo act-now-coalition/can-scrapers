5722320e6e19411ab56152a32538f755ee397408
Chase Coleman <cc7768@gmail.com>
2020-11-28 14:09:39 -0500

    HHS temp commit

--- can_tools/scrapers/official/federal/HHS/__init__.py
+++ can_tools/scrapers/official/federal/HHS/__init__.py
@@ -1,1 +1,2 @@
 from can_tools.scrapers.official.federal.HHS.hhs_state import HHSTimeSeries
+# https://healthdata.gov/dataset/covid-19-reported-patient-impact-and-hospital-capacity-state-timeseries/resource/3ce11e2c
