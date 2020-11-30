71208f14b150cc11d7d2ba44ca08bcdd10cde731
Christopher Turner <turnerc@usc.edu>
2020-11-30 13:00:03 -0800

    Further progress with hospital data

--- can_tools/scrapers/official/PA/pa_state.py
+++ can_tools/scrapers/official/PA/pa_state.py
@@ -4,6 +4,7 @@ import us
 from can_tools.scrapers import CMU, DatasetBase
 from can_tools.scrapers.official.base import ArcGIS
 
+from typing import Dict
 
 class Pennsylvania(ArcGIS, DatasetBase):
     """
@@ -15,10 +16,10 @@ class Pennsylvania(ArcGIS, DatasetBase):
     state_fips = int(us.states.lookup("Pennsylvania").fips)
     source = "https://experience.arcgis.com/experience/ed2def13f9b045eda9f7d22dbc9b500e"
 
-    def fetch(self):
+    def fetch(self) -> Dict:
         raw_data = {}
         raw_data["cases_deaths"] = self.get_all_jsons("COVID_PA_Counties", 0, 1)
-        raw_data["hospitals"] = self.get_all_jsons("covid_hosp", 0, 1)
+        raw_data["hospitals"] = self.get_all_jsons("covid_hosp_single_day", 0, 1)
         return raw_data
 
     def normalize(self, data) -> pd.DataFrame:
@@ -26,7 +27,6 @@ class Pennsylvania(ArcGIS, DatasetBase):
         hospitals = self.normalize_hospitals(data["hospitals"])
 
         out = pd.concat([cases_deaths, hospitals], axis=0, ignore_index=True, sort=True)
-        out["vintage"] = self._retrieve_vintage()
         return out
 
     def normalize_cases_deaths(self, data) -> pd.DataFrame:
@@ -67,7 +67,7 @@ class Pennsylvania(ArcGIS, DatasetBase):
         cols_to_keep = [
             "vintage",
             "dt",
-            "location",
+            "county",
             "category",
             "measurement",
             "unit",
@@ -80,7 +80,40 @@ class Pennsylvania(ArcGIS, DatasetBase):
         return out.loc[:, cols_to_keep]
 
     def normalize_hospitals(self, data) -> pd.DataFrame:
-        return 1
+        df = self.arcgis_jsons_to_df(data)
+        df.columns = [x.lower() for x in list(df)]
+
+        crename = {
+            "med_total": CMU(category="hospital_beds_capacity", measurement="current", unit="beds"),
+            "covid_patients": CMU(category="hospital_beds_in_use_covid", measurement="current", unit="beds"),
+            "icu_total": CMU(category="icu_beds_capacity", measurement="current", unit="beds"),
+            "icu_avail": CMU(category="icu_beds_available", measurement="current", unit="beds")
+        }
+
+        out = (
+            df.melt(id_vars=["county"], value_vars=crename.keys())
+            .assign(
+                dt = self._retrieve_dt("US/Eastern"), vintage=self._retrieve_vintage()
+            )
+            .dropna()
+        )
+        out.loc[:, "value"] = pd.to_numeric(out["value"])
+
+        out = self.extract_CMU(out,crename)
+
+        cols_to_keep = [
+            "vintage",
+            "dt",
+            "county",
+            "category",
+            "measurement",
+            "unit",
+            "age",
+            "race",
+            "sex",
+            "value",
+        ]
+        return out.loc[:, cols_to_keep]
 
     def validate(self, df, df_hist):
         return True
