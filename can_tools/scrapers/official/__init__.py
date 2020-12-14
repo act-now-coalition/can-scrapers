482131f6742089285f2b98613c0a669608280e59
ChrisBremer <75241831+ChrisBremer@users.noreply.github.com>
2020-12-13 22:45:24 -0700

    Update __init__.py

--- can_tools/scrapers/official/__init__.py
+++ can_tools/scrapers/official/__init__.py
@@ -16,8 +16,10 @@ from can_tools.scrapers.official.PA import (
     PennsylvaniaHospitals,
 )
 
+
 # from can_tools.scrapers.official.IL import IllinoisDemographics, IllinoisHistorical
 # from can_tools.scrapers.official.MA import Massachusetts
+from can_tools.scrapers.official.MD import Maryland
 from can_tools.scrapers.official.NY import NewYorkTests
 from can_tools.scrapers.official.TX import TexasCasesDeaths, TexasTests
 from can_tools.scrapers.official.WI import Wisconsin
