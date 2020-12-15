fdde2b8b75c33936c90d4d8fc5759b91ab65d10f
Chase Coleman <cc7768@gmail.com>
2020-12-14 19:28:29 -0500

    Small updates to WI scrapers

--- can_tools/scrapers/official/WI/__init__.py
+++ can_tools/scrapers/official/WI/__init__.py
@@ -1,2 +1,2 @@
-from can_tools.scrapers.official.WI.wi_state import WisconsinCounties
+from can_tools.scrapers.official.WI.wi_county import WisconsinCounties
 from can_tools.scrapers.official.WI.wi_state import WisconsinState
