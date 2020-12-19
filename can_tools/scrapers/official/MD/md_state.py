63de6fd606a83c0aae63793a2bb4d6ed8cd374bf
ChrisBremer <75241831+ChrisBremer@users.noreply.github.com>
2020-12-18 21:57:40 -0700

    Adding a rollup of counties to state-level
     (state-summary data is disparate on the ArcGIS platform today)
    
    simple script to summarize the county-level and push to the state FIPS code

---
+++ can_tools/scrapers/official/MD/md_state.py
@@ -0,0 +1,95 @@
+import pandas as pd
+
+import us
+from can_tools.scrapers import CMU, DatasetBase
+from can_tools.scrapers.official.base import ArcGIS
+
+
+class Maryland(ArcGIS, DatasetBase):
+    """
+    Fetch county level covid data from Maryland's ARCGIS dashboard, roll up to state level.
+    """
+
+    ARCGIS_ID = "njFNhDsUCentVYJW"
+    has_location = True
+    location_type = "county"
+    state_fips = int(us.states.lookup("Maryland").fips)
+    source = "https://coronavirus.maryland.gov/"
+
+    def fetch(self):
+        return self.get_all_jsons(
+            "MDH_COVID_19_Dashboard_Feature_Layer_Counties_MEMA", 0, ""
+        )
+
+    def normalize(self, data):
+        df = self.arcgis_jsons_to_df(data)
+        df.columns = [x.lower() for x in list(df)]
+        df["location"] = self.state_fips
+        df.drop(
+            columns=[
+                "objectid",
+                "county",
+                "district",
+                "county_fip",
+                "countynum",
+                "shape__area",
+                "shape__length",
+            ],
+            inplace=True,
+        )
+        df = df.groupby(["location"], as_index=False)[
+            "totalcasecount",
+            "totaldeathcount",
+            "probdeaths",
+            "eocstatus",
+            "total_pop_tested",
+            "daily_testing_vol",
+            "total_testing_vol",
+        ].sum()
+        crename = {
+            "totalcasecount": CMU(
+                category="cases", measurement="cumulative", unit="people"
+            ),
+            "totaldeathcount": CMU(
+                category="deaths", measurement="cumulative", unit="people"
+            ),
+            "total_pop_tested": CMU(
+                category="pcr_tests_total",
+                measurement="cumulative",
+                unit="unique_people",
+            ),
+            "total_testing_vol": CMU(
+                category="pcr_tests_total",
+                measurement="cumulative",
+                unit="specimens",
+            ),
+            "daily_testing_vol": CMU(
+                category="pcr_tests_total",
+                measurement="new",
+                unit="specimens",
+            ),
+        }
+
+        out = (
+            df.melt(id_vars=["location"], value_vars=crename.keys())
+            .assign(
+                dt=self._retrieve_dt("US/Eastern"), vintage=self._retrieve_vintage()
+            )
+            .dropna()
+        )
+        out.loc[:, "value"] = pd.to_numeric(out["value"])
+        # Extract category information and add other variable context
+        out = self.extract_CMU(out, crename)
+        cols_to_keep = [
+            "vintage",
+            "dt",
+            "location",
+            "category",
+            "measurement",
+            "unit",
+            "age",
+            "race",
+            "sex",
+            "value",
+        ]
+        return out.loc[:, cols_to_keep]
