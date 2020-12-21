08961e6c9d6237ae841cdcb72fe1aa5c401729b2
Christopher Turner <turnerc@usc.edu>
2020-12-20 22:29:37 -0800

    Beginning fetch and (pre)normalize

--- can_tools/scrapers/official/IA/ia_state.py
+++ can_tools/scrapers/official/IA/ia_state.py
@@ -31,6 +31,23 @@ class IowaCasesDeaths(ArcGIS):
         "value",
     ]
 
+    def fetch(self) -> Any:
+        return self.get_all_jsons(self.service, 0, 1)
+
+    def pre_normalize(self, data) -> pd.DataFrame:
+        df = self.arcgis_jsons_to_df(data)
+
+    def normalize(self, data) -> pd.DataFrame:
+        # Normalize data, which is dependent on the current class
+        out = self.pre_normalize(data)
+
+        out["vintage"] = self._retrieve_vintage()
+        return out
+
 
 class IowaHospitals(IowaCasesDeaths):
+
     service: str = "COVID19_RMCC_Hospitalization"
+
+    def pre_normalize(self, data) -> pd.DataFrame:
+        df = self.arcgis_jsons_to_df(data)
