ce5a90345a7b64f59d52300b94de506d9e0203d1
Sean McClure <mcclure_sean@wheatoncollege.edu>
2020-12-21 18:32:57 -0500

    implement DC trackers
    implement cases, deaths, and general/hospital trackers for DC.
    
    updated variable files as needed

---
+++ can_tools/scrapers/official/DC/dc_base.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import us
+import lxml.html
+import requests
+from abc import abstractmethod
+from can_tools.scrapers.official.base import StateDashboard
+
+class DCBase(StateDashboard):
+    has_location = False
+    state_fips = us.states.lookup("DC").fips
+    location_type = "state"
+    source = "https://coronavirus.dc.gov/page/coronavirus-data"
+
+    def fetch(self) -> pd.ExcelFile:
+        """
+        locate most recent data export, download excel file
+
+        Returns
+        -------
+        df: pd.ExcelFile
+            A pandas Excel File
+        """
+        res = requests.get(self.source)
+        if not res.ok:
+            raise ValueError("Could not fetch source of DC page")
+        tree = lxml.html.fromstring(res.content)
+        links = tree.xpath('//a[contains(text(), "Download copy")]/@href') #find all download links
+
+        #selects most recent (yesterdays) file by taking the first off the stack
+        if links[0].startswith('https'):
+            xl_src = links[0]
+        elif links[0].startswith('/'):
+            xl_src = 'https://coronavirus.dc.gov' + links[0]
+        else: raise ValueError("Could not parse download link")
+
+        #download xlsx from selected link
+        xl = requests.get(xl_src) 
+        if not xl.ok:
+            raise ValueError("Could not fetch download file")
+
+        return pd.ExcelFile(xl.content) #read file into pd excel object
+    
+    def _reshape(self, data: pd.DataFrame, map: dict) -> pd.DataFrame:
+        """
+        Function to prep data for put() function. renames and adds columns according to CMU (map) entries
+            
+            Accepts
+            ------- 
+                data: df w/ column names according the map parameter 
+                    example of format: 
+                                dt   Variable Name  ...         location_name
+                    0   2020-03-07  Variable Value  ...  District of Columbia
+                    ...
+                map: dictionary with CMU keys/values
+            
+            Returns
+            ------- 
+                pd.Dataframe: dataframe ready for put() function     
+        """
+
+        out = data.melt(
+            id_vars=["dt","location_name"], value_vars=map.keys()
+        ).dropna()
+        out.loc[:, "value"] = pd.to_numeric(out["value"])
+
+        out = self.extract_CMU(out, map)
+        out["vintage"] = self._retrieve_vintage()
+
+        cols_to_keep = [
+            "vintage",
+            "dt",
+            "location_name",
+            "category",
+            "measurement",
+            "unit",
+            "age",
+            "race",
+            "sex",
+            "value",
+            "variable"
+        ]
+
+        return out.loc[:, cols_to_keep] 
+
+    @abstractmethod
+    def _wrangle(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Parent function to re-structure df into standard form. 
+        Used in child class to transpose and clean data returned from DC's excel data exports 
+            Accepts
+            ------- 
+                data: pd.Dataframe 
+            Returns
+            ------- 
+                pd.DataFrame    
+        """
+
+
+
+
---
+++ can_tools/scrapers/official/DC/dc_deaths.py
@@ -0,0 +1,141 @@
+import pandas as pd
+from can_tools.scrapers import CMU
+from can_tools.scrapers.official.DC.dc_base import DCBase
+
+class DCDeaths(DCBase):
+    def _wrangle(self, data):
+        """
+        inherited helper function to transform excel data into standard dataframe format
+    
+        accepts: Pd.Dataframe
+        returns: Pd.Dataframe
+        """
+        df = data.transpose()
+        df =  ( df.rename(columns=df.iloc[0])
+                .drop(df.index[0]) 
+                .rename_axis("dt").reset_index() #make rownames into column 
+            ) 
+        df["location_name"] = "District of Columbia"
+        return df
+    
+    def normalize(self, data):
+        #retrieve data and convert dataframe structure
+        df_age = self._wrangle(data.parse('Lives Lost by Age'))
+        df_sex = self._wrangle(data.parse('Lives Lost by Sex'))
+        df_race = self._wrangle(data.parse('Lives Lost by Race'))
+
+        #maps for each df
+        crename_age = {
+            "<19": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                age="0-19",
+            ),
+            "20-29": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                age="20-29",
+            ),
+            "30-39": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                age="30-39",
+            ),
+            "40-49": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                age="40-49",
+            ),
+            "50-59": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                age="50-59",
+            ),
+            "60-69": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                age="60-69",
+            ),
+            "70-79": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                age="70-79",
+            ),
+            "80+": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                age="80_plus",
+            ),
+            "All": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+            ),
+        }
+        crename_sex = {
+            "Female": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                sex="female",
+            ),
+            "Male": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                sex="male",
+            ),
+        }
+        crename_race = {
+            "Asian": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                race="asian",
+            ),
+            "Black/African American": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                race="black",
+            ),
+            "Hispanic/Latinx": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                race="hispanic",
+            ),
+            "Non-Hispanic White": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                race="white",
+            ),
+            "Unknown": CMU(
+                category="deaths",
+                measurement="cumulative",
+                unit="people",
+                race="unknown",
+            ),           
+        }
+        
+        #rename and add columns according to map
+        df_age = self._reshape(df_age,crename_age)
+        df_sex = self._reshape(df_sex,crename_sex)
+        df_race = self._reshape(df_race,crename_race)
+
+        #combine all into one df
+        df = pd.DataFrame()
+        df = (df.append(df_age, ignore_index = True)
+                .append(df_sex, ignore_index = True)
+                .append(df_race, ignore_index = True))
+
+        return df
