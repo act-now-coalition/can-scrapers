ca3665c7790f15ac0b0e2fb4e16fe8ea8b75c2fa
Christopher Turner <turnerc@usc.edu>
2021-01-09 00:08:54 -0800

    Progress on IowaCasesDeaths

--- can_tools/scrapers/official/IA/ia_state.py
+++ can_tools/scrapers/official/IA/ia_state.py
@@ -29,13 +29,47 @@ class IowaCasesDeaths(ArcGIS):
         "race",
         "sex",
         "value",
+        "fips"
     ]
 
     def fetch(self) -> Any:
-        return self.get_all_jsons(self.service, 0, 1)
+        return self.get_all_jsons(self.service, 0, "")
 
     def pre_normalize(self, data) -> pd.DataFrame:
         df = self.arcgis_jsons_to_df(data)
+
+        # Make columns names all-lowercase
+        df.columns = [x.lower() for x in list(df)]
+        df = df.rename(
+            columns={"name": "location_name", "last_updated": "dt"}
+        )
+
+        crename = {
+            "confirmed": CMU(
+                category="cases",
+                measurement="cumulative",
+                unit="unique_people",
+            ),
+            "deaths": CMU(
+                category="deaths", measurement="cumulative", unit="unique_people"
+            ),
+            "individuals_tested": CMU(
+                category="unspecified_tests_total",
+                measurement="cumulative",
+                unit="unique_people",
+            ),
+        }
+
+        out = (
+            df.melt(id_vars=["dt", "location_name","fips"], value_vars=crename.keys())
+            .assign(dt=self._retrieve_dt("US/Central"))
+            .dropna()
+        )
+
+        out = self.extract_CMU(out, crename)
+
+        return out.loc[:, self.cols_to_keep].query("fips != '0'")
+
 
     def normalize(self, data) -> pd.DataFrame:
         # Normalize data, which is dependent on the current class
