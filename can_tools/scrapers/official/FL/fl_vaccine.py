ffba5c2545bfb92a0a371d42e49a429c9ba62a55
smcclure17 <sean.mcclure17@gmail.com>
2021-01-19 19:26:41 -0500

    create county tracker complete

--- can_tools/scrapers/official/FL/fl_vaccine.py
+++ can_tools/scrapers/official/FL/fl_vaccine.py
@@ -1,29 +1,26 @@
 import pandas as pd
-import tabula
+from tabula import read_pdf
 import requests
 import us
 
 from can_tools.scrapers import CMU
 from can_tools.scrapers.official.base import StateDashboard
 
-class FloridaVaccine():
+class FloridaVaccine(StateDashboard):
     has_location = False
-    state_fips = us.states.lookup("Florida").fips
-    location_type = "state"
-    source = "source"
+    source = "https://bi.ahca.myflorida.com/t/ABICC/views/Public/HospitalBedsCounty"
+    location_type = "county"
+    state_fips = int(us.states.lookup("Florida").fips)
 
     def fetch(self):
         fetch_url = "http://ww11.doh.state.fl.us/comm/_partners/covid19_report_archive/vaccine/vaccine_report_latest.pdf"
-        res = requests.get(fetch_url)
-        if not res.ok:
-            print("failure")
-
-        return res.content
+        return read_pdf(fetch_url, pages=2, area=[134,77,1172.16,792])  # Get table by it's location in the PDF
 
     def normalize(self, data):
-        df = tabula.read_pdf("http://ww11.doh.state.fl.us/comm/_partners/covid19_report_archive/vaccine/vaccine_report_latest.pdf", 
-        pages=2, area=[134,77,1172.16,792]) #get table by it's location in the PDF
-        df = pd.concat(df)
+        df = pd.concat(data).rename(columns={"County of residence":"location_name"})
+        # Ignore data from unknown region (no fips code)
+        df = df[(df["location_name"] != 'Unknown') & (df["location_name"] != 'Out-Of-State')].head(5)
+        df.loc[df["location_name"] == "Desoto", "location_name"] = "DeSoto"
         
         crename = {
             'First dose': CMU(
@@ -31,34 +28,57 @@ class FloridaVaccine():
                 measurement="new",
                 unit="people",
                 ),
-            'complete': CMU(
+            'Series\rcomplete': CMU(
                 category="total_vaccine_completed",
                 measurement="new",
                 unit="people",
                 ),
-            'vaccinatedle': CMU(
+            'Total people\rvaccinated': CMU(
                 category="total_vaccine_doses_administered",
                 measurement="new",
                 unit="doses",
                 ),
             'First dose.1': CMU(
                 category="total_vaccine_initiated",
-                measurement="new",
+                measurement="cumulative",
                 unit="people",
                 ),
-            'complete.1': CMU(
+            'Series\rcomplete.1': CMU(
                 category="total_vaccine_completed",
-                measurement="new",
+                measurement="cumulative",
                 unit="people",
                 ),
-            'vaccinated.1': CMU(
+            'Total people\rvaccinated.1': CMU(
                 category="total_vaccine_doses_administered",
-                measurement="new",
+                measurement="cumulative",
                 unit="doses",
                 ),
         }
-        
-        return df
+
+        out = df.melt(
+            id_vars=["location_name"], value_vars=crename.keys()
+        ).dropna()
+        # out.loc[:, "value"] = pd.to_numeric(out["value"].str.replace(',','').str.replace('nan',None)) #FIX CONVERSIONS (this is what throws error) 
+        out = self.extract_CMU(out, crename)
+
+        out["vintage"] = self._retrieve_vintage()
+        out["dt"] = self._retrieve_dtm1d("US/Eastern")
+
+        cols_to_keep = [
+            "vintage",
+            "dt",
+            "location_name",
+            "category",
+            "measurement",
+            "unit",
+            "age",
+            "race",
+            "ethnicity",
+            "sex",
+            "value",
+        ]
+
+        return out.loc[:, cols_to_keep]
 
  
  
