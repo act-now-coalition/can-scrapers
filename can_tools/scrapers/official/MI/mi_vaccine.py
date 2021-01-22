51d735818208f084391c89b63f5fcd5989556691
Spencer Lyon <spencerlyon2@gmail.com>
2021-01-21 23:22:43 -0500

    add totals to mi vaccine scraper

--- can_tools/scrapers/official/MI/mi_vaccine.py
+++ can_tools/scrapers/official/MI/mi_vaccine.py
@@ -18,6 +18,14 @@ class MichiganVaccineCounty(StateDashboard):
     def normalize(self, data: pd.DataFrame) -> pd.DataFrame:
         # date is written out in first column name
         data["variable"] = data["Vaccine Type"] + data["Dose Number"]
+        data["variable"] = data["variable"].str.replace(" ", "")
+
+        def _make_cmu(cat):
+            return CMU(
+                category=cat,
+                measurement="cumulative",
+                unit="people",
+            )
 
         colnames = {
             "Person's Residence in County": "location_name",
@@ -25,25 +33,16 @@ class MichiganVaccineCounty(StateDashboard):
             "Number of Doses": "value",
         }
         cmus = {
-            "ModernaFirst Dose": CMU(
-                category="moderna_vaccine_initiated",
+            "ModernaFirstDose": _make_cmu("moderna_vaccine_initiated"),
+            "ModernaSecondDose": _make_cmu("moderna_vaccine_completed"),
+            "PfizerFirstDose": _make_cmu("pfizer_vaccine_initiated"),
+            "PfizerSecondDose": _make_cmu("pfizer_vaccine_completed"),
+            "total_initiated": _make_cmu("total_vaccine_initiated"),
+            "total_completed": _make_cmu("total_vaccine_completed"),
+            "total": CMU(
+                category="total_vaccine_doses_administered",
                 measurement="cumulative",
-                unit="people",
-            ),
-            "ModernaSecond Dose": CMU(
-                category="moderna_vaccine_completed",
-                measurement="cumulative",
-                unit="people",
-            ),
-            "PfizerFirst Dose": CMU(
-                category="pfizer_vaccine_initiated",
-                measurement="cumulative",
-                unit="people",
-            ),
-            "PfizerSecond Dose": CMU(
-                category="pfizer_vaccine_completed",
-                measurement="cumulative",
-                unit="people",
+                unit="doses",
             ),
         }
         not_counties = ["No County", "Detroit"]  # noqa
@@ -57,32 +56,45 @@ class MichiganVaccineCounty(StateDashboard):
                 index=["dt", "location_name"],
                 columns="variable",
                 values="value",
-                aggfunc="sum"
+                aggfunc="sum",
             )
             .fillna(0)
             .astype(int)
+            .assign(
+                total_initiated=lambda x: x.eval("ModernaFirstDose + PfizerFirstDose"),
+                total_completed=lambda x: x.eval(
+                    "ModernaSecondDose + PfizerSecondDose"
+                ),
+            )
+            .assign(
+                total=lambda x: x.eval("total_initiated + total_completed"),
+            )
         )
 
         # now we need to reindex to fill in all dates -- fill missing with 0
         dates = pd.Series(df.index.get_level_values("dt")).agg(["min", "max"])
-        new_index = pd.MultiIndex.from_product([
-            pd.date_range(*dates),
-            df.index.get_level_values("location_name").unique()
-        ], names=["dt", "location_name"])
+        new_index = pd.MultiIndex.from_product(
+            [
+                pd.date_range(*dates),
+                df.index.get_level_values("location_name").unique(),
+            ],
+            names=["dt", "location_name"],
+        )
 
         return (
-            df
-            .reindex(new_index, fill_value=0)         # fill in missing dates
-            .sort_index()                             # make sure we are sorted
-            .unstack(level=["location_name"])         # make index=dt, columns=[variable,loc_name]
-            .cumsum()                                 # compute cumulative sum
+            df.reindex(new_index, fill_value=0)  # fill in missing dates
+            .sort_index()  # make sure we are sorted
+            .unstack(
+                level=["location_name"]
+            )  # make index=dt, columns=[variable,loc_name]
+            .cumsum()  # compute cumulative sum
             .stack(level=[0, 1])  # long form Series
-            .rename("value")                          # name the series
-            .reset_index()                            # convert to long form df
-            .assign(                                  # fill fix value
+            .rename("value")  # name the series
+            .reset_index()  # convert to long form df
+            .assign(  # fill fix value
                 value=lambda x: pd.to_numeric(x.loc[:, "value"]),
                 vintage=self._retrieve_vintage(),
             )
-            .pipe(self.extract_CMU, cmu=cmus)         # extract CMUs
-            .drop(["variable"], axis=1)               # drop variable
+            .pipe(self.extract_CMU, cmu=cmus)  # extract CMUs
+            .drop(["variable"], axis=1)  # drop variable
         )
