d628ffb564225df53b3ba293c7c4393bedec21e2
Spencer Lyon <spencerlyon2@gmail.com>
2021-01-22 16:18:27 -0500

    add caddyfile

--- services/prefect/setup_gcp_instance.sh
+++ services/prefect/setup_gcp_instance.sh
@@ -74,3 +74,10 @@ mount ~/scraper-outputs
 
 # add environment variable for the scraper-outputs
 echo "export DATAPATH=/home/sglyon/scraper-outputs" | tee -a ~/.bashrc > /dev/nul
+
+# setup caddy for reverse proxy
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/cfg/gpg/gpg.155B6D79CA56EA34.key' | sudo apt-key add -
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/cfg/setup/config.deb.txt?distro=debian&version=any-version' | sudo tee -a /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install -y caddy
