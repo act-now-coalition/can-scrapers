ccef5a1adb005a2d67e24c9c4103fb645cfb9727
ChrisBremer <75241831+ChrisBremer@users.noreply.github.com>
2021-01-23 16:09:35 -0700

    Update DE_state.py

--- can_tools/scrapers/official/DE/DE_state.py
+++ can_tools/scrapers/official/DE/DE_state.py
@@ -162,6 +162,7 @@ class DelawareStateData(StateDashboard):
             "unit",
             "age",
             "race",
+            "ethnicity",
             "sex",
             "value",
         ]
