f43d0b075a9e2fc3cc8fc184e2abec59a2dab5a8
Spencer Lyon <spencerlyon2@gmail.com>
2021-02-22 12:35:09 -0500

    WIP tableau scraper template/cli

---
+++ helpers/templates/tableau.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import us
+
+from can_tools.scrapers.base import CMU
+from can_tools.scrapers.official.base import TableauDashboard
+
+
+class {{ scraper.name }}(TableauDashboard):
+    has_location = False
+    source = "{{ scraper.source }}"
+    source_name = "{{ scraper.source_name }}"
+    state_fips = int(us.states.lookup("{{ scraper.state_name }}").fips)
+    location_type = "county"
+    baseurl = "{{ scraper.baseurl }}"
+    viewPath = "{{ scraper.viewPath }}"
+
+    data_tableau_table = "{{ scraper.data_tableau_table }}"
+    location_name_col = "{{ scraper.location_name_col }}"
+    timezone = "{{ scraper.timezone }}"
+
+    # map wide form column names into CMUs
+    cmus = {
+    }
