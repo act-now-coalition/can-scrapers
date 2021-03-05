d210806a85fc35419b255aa47f9fd49d39635cac
Sean McClure <sean.mcclure17@gmail.com>
2021-03-05 17:49:44 -0500

    add files i forgot to stage

---
+++ can_tools/scrapers/official/WI/wi_county_vaccine.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import us
+
+from can_tools.scrapers.base import CMU
+from can_tools.scrapers.official.base import TableauDashboard
+
+
+class WisconsinVaccineCounty(TableauDashboard):
+    has_location = False
+    source = "https://www.dhs.wisconsin.gov/covid-19/vaccine-data.htm#summary"
+    source_name = "Wisconsin Department of Health Services"
+    state_fips = int(us.states.lookup("Wisconsin").fips)
+    location_type = "county"
+    baseurl = "https://bi.wisconsin.gov/t/DHS"
+    viewPath = "VaccinesAdministeredtoWIResidents/VaccinatedWisconsin-County"
+
+    data_tableau_table = "Map"
+    location_name_col = "County-alias"
+    timezone = "US/Central"
+
+    # map wide form column names into CMUs
+    cmus = {
+        "SUM(Number With One Dose)-alias": CMU(
+            category="total_vaccine_initiated",
+            measurement="cumulative",
+            unit="people",
+        ),
+        "SUM(Number With Two Doses)-alias": CMU(
+            category="total_vaccine_completed",
+            measurement="cumulative",
+            unit="people",
+        ),
+    }
+
+    def normalize(self, df: pd.DataFrame) -> pd.DataFrame:
+        # county names (converted to title case)
+        df["location_name"] = df[self.location_name_col].str.title()
+        df["location_name"] = (
+            df["location_name"].str.rsplit(" ", 1).str[0]
+        )  # remove "county" from end of name
+        df = df.replace(
+            {"location_name": {"St Croix": "St. Croix", "Fond Du Lac": "Fond du Lac"}}
+        )  # fix incorrect formatting
+
+        # parse out data columns
+        value_cols = list(set(df.columns) & set(self.cmus.keys()))
+        assert len(value_cols) == len(self.cmus)
+
+        df = (
+            df.melt(id_vars=["location_name"], value_vars=value_cols)
+            .dropna()
+            .assign(
+                dt=self._retrieve_dt(self.timezone),
+                vintage=self._retrieve_vintage(),
+                value=lambda x: pd.to_numeric(
+                    x["value"].astype(str).str.replace(",", "")
+                ),
+            )
+            .pipe(self.extract_CMU, cmu=self.cmus)
+            .drop(["variable"], axis=1)
+        )
+        return df
---
+++ can_tools/scrapers/official/WI/wi_demographic_vaccine.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import us
+
+from can_tools.scrapers.base import CMU
+from can_tools.scrapers.official.base import TableauDashboard
+from can_tools.scrapers.official.WI.wi_county_vaccine import WisconsinVaccineCounty
+
+
+class WisconsinVaccineAge(WisconsinVaccineCounty):
+    data_tableau_table = "Age vax/unvax County"
+    location_name_col = "AGG(Geography TT)-alias"
+    location_type = "state"
+
+    # map wide form column names into CMUs
+    cmus = {
+        "SUM(Initiation or completed count for TT)-alias": CMU(
+            category="total_vaccine_initiated",
+            measurement="cumulative",
+            unit="people",
+        )
+    }
+
+    def _get_demographic(
+        self, df: pd.DataFrame, demo: str, demo_col_name: str
+    ) -> pd.DataFrame:
+        """
+        description: a general "normalize" function to avoid extra/copied code
+
+        params:
+            demo: the demographic as labeled according to CMU (age,sex,race, etc...)
+            demo_col_name: the name of the demographic column from the fetched data
+
+        returns: normalized data in long format
+        """
+
+        # county names (converted to title case)
+        df["location_name"] = df[self.location_name_col].str.title()
+        # remove "county" from end of name
+        df["location_name"] = df["location_name"].str.rsplit(" ", 1).str[0]
+        # fix county names
+        df = df.replace(
+            {"location_name": {"St Croix": "St. Croix", "Fond Du Lac": "Fond du Lac"}}
+        )
+
+        # parse out data columns
+        value_cols = list(set(df.columns) & set(self.cmus.keys()))
+        assert len(value_cols) == len(self.cmus)
+
+        df = (
+            df.melt(id_vars=[demo_col_name, "location_name"], value_vars=value_cols)
+            .dropna()
+            .assign(
+                dt=self._retrieve_dt(self.timezone),
+                vintage=self._retrieve_vintage(),
+                value=lambda x: pd.to_numeric(
+                    x["value"].astype(str).str.replace(",", "")
+                ),
+            )
+            .pipe(self.extract_CMU, cmu=self.cmus)
+        )
+        df[demo] = df[demo_col_name]
+        return df.drop(["variable", demo_col_name], axis=1)
+
+    def normalize(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = self._get_demographic(df, "age", "Age-value")
+        df = df.replace({"age": {"65+": "65_plus"}})
+        return df
+
+
+class WisconsinVaccineRace(WisconsinVaccineAge):
+    data_tableau_table = "Race vax/unvax county"
+    location_name_col = "AGG(Geography TT)-alias"
+
+    def normalize(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = self._get_demographic(df, "race", "Race-value")
+        rm = {"1": "white", "2": "black", "3": "native_american", "4": "asian"}
+        return df.replace({"race": rm})
+
+
+class WisconsinVaccineSex(WisconsinVaccineAge):
+    data_tableau_table = "Sex vax/unvax county"
+    location_name_col = "AGG(Geography TT)-alias"
+
+    def normalize(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = self._get_demographic(df, "sex", "Sex-value")
+        df["sex"] = df["sex"].str.lower()
+        return df
+
+
+class WisconsinVaccineEthnicity(WisconsinVaccineAge):
+    data_tableau_table = "Ethnicity vax/unvax county"
+    location_name_col = "AGG(Geography TT)-alias"
+
+    def normalize(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = self._get_demographic(df, "ethnicity", "Ethnicity-value")
+        df["ethnicity"] = df["ethnicity"].str.lower()
+        return df
