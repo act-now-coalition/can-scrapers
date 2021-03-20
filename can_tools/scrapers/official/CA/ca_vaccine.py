28ecbc8fc86e586f0eb3f1d10e554b39f8171d69
Chris Kelly <ghop02@gmail.com>
2021-03-20 14:33:07 -0400

    Uses new california spreadsheet of county level vaccination data

--- can_tools/scrapers/official/CA/ca_vaccine.py
+++ can_tools/scrapers/official/CA/ca_vaccine.py
@@ -1,57 +1,41 @@
 import pandas as pd
 import us
 
-from can_tools.scrapers.base import CMU
-from can_tools.scrapers.official.base import TableauDashboard
+from can_tools.scrapers.official.base import StateDashboard
+from can_tools.scrapers import variables
 
 
-class CaliforniaVaccineCounty(TableauDashboard):
+class CaliforniaVaccineCounty(StateDashboard):
     has_location = False
-    source = "https://covid19.ca.gov/vaccines/#California-vaccines-dashboard"
-    source_name = "Official California State Government Website"
+    source = (
+        "https://data.chhs.ca.gov/dataset/vaccine-progress-dashboard/"
+        "resource/130d7ba2-b6eb-438d-a412-741bde207e1c"
+    )
+    source_name = "California Health & Human Services Agency"
     state_fips = int(us.states.lookup("California").fips)
     location_type = "county"
 
-    source_url = (
-        "https://public.tableau.com/views/COVID-19VaccineDashboardPublicv2/Vaccine?"
-        ":embed=y&:showVizHome=no&:host_url=https://public.tableau.com/&:embed_code_version=3"
-        "&:tabs=no&:toolbar=yes&:animate_transition=yes&:display_static_image=no"
-        "&:display_spinner=no&:display_overlay=yes&:display_count=yes"
-        "&:language=en&publish=yes&:loadOrderID=0"
+    url = (
+        "https://data.chhs.ca.gov/dataset/e283ee5a-cf18-4f20-a92c-ee94a2866ccd/resource/"
+        "130d7ba2-b6eb-438d-a412-741bde207e1c/download/covid19vaccinesbycounty.csv"
     )
-    baseurl = "https://public.tableau.com"
 
-    viewPath = "COVID-19VaccineDashboardPublicv2/Vaccine"
-
-    cmus = {
-        "SUM(Dose Administered)-alias": CMU(
-            category="total_vaccine_doses_administered",
-            measurement="cumulative",
-            unit="doses",
-        ),
+    variables = {
+        "cumulative_fully_vaccinated": variables.FULLY_VACCINATED_ALL,
+        "cumulative_at_least_one_dose": variables.INITIATING_VACCINATIONS_ALL,
+        "cumulative_total_doses": variables.TOTAL_DOSES_ADMINISTERED_ALL,
     }
 
-    county_column = "County-alias"
-
     def fetch(self) -> pd.DataFrame:
-        return self.get_tableau_view(self.source_url)["County Admin Map"]
+        return pd.read_csv(self.url)
 
-    def normalize(self, df: pd.DataFrame) -> pd.DataFrame:
-        # county names (converted to title case)
-        df["location_name"] = df[self.county_column].str.title()
-
-        # parse out 1st/2nd dose columns
-        value_cols = list(set(df.columns) & set(self.cmus.keys()))
-        assert len(value_cols) == 1
-
-        return (
-            df.melt(id_vars=["location_name"], value_vars=value_cols)
-            .dropna()
-            .assign(
-                dt=self._retrieve_dt("US/Pacific"),
-                vintage=self._retrieve_vintage(),
-                value=lambda x: pd.to_numeric(x.loc[:, "value"]),
-            )
-            .pipe(self.extract_CMU, cmu=self.cmus)
-            .drop(["variable"], axis=1)
+    def normalize(self, data: pd.DataFrame) -> pd.DataFrame:
+        data = self._rename_or_add_date_and_location(
+            data,
+            location_name_column="county",
+            date_column="administered_date",
+            location_names_to_drop=["Outside California", "Unknown"],
         )
+
+        data = self._reshape_variables(data, self.variables)
+        return data
