de812727d11e2a6bc4f087eea834ea8d1d0a53c7
Trevor Lyon <trevor.lyon@icloud.com>
2021-03-29 14:44:28 -0300

    FEAT: Add DC all vaccines

--- can_tools/scrapers/official/DC/dc_vaccines.py
+++ can_tools/scrapers/official/DC/dc_vaccines.py
@@ -3,6 +3,7 @@ import us
 
 from can_tools.scrapers.base import CMU
 from can_tools.scrapers.official.base import TableauDashboard
+from can_tools.scrapers import variables
 
 
 class DCVaccineSex(TableauDashboard):
@@ -63,3 +64,31 @@ class DCVaccineSex(TableauDashboard):
         )
 
         return out
+
+
+class DCVaccine(TableauDashboard):
+    has_location = True
+    source = "https://coronavirus.dc.gov/data/vaccination"
+    source_name = "DC Health"
+    state_fips = int(us.states.lookup("District of Columbia").fips)
+    location_type = "state"
+    baseurl = "https://dataviz1.dc.gov/t/OCTO"
+    viewPath = "Vaccine_Public/Administration"
+    data_tableau_table = "TimeTable"
+
+    variables = {
+        'FULLY VACCINATED': variables.FULLY_VACCINATED_ALL,
+        'PARTIALLY/FULLY VACCINATED': variables.INITIATING_VACCINATIONS_ALL
+    }
+
+    def normalize(self, data):
+        df = data
+        df['Measure Values-alias'] =  pd.to_numeric(df['Measure Values-alias'].str.replace(',',''), errors='coerce')
+        df = df.loc[df['Resident_Type-value'] == 'DC Resident'][['Measure Values-alias', 'Measure Names-alias']]
+        df['location'] = self.state_fips
+        df =  df.pivot(index='location', columns='Measure Names-alias', values='Measure Values-alias').reset_index().rename_axis(None, axis=1)
+
+        renamed = self._rename_or_add_date_and_location(df, location_column='location', timezone="US/Eastern")
+        out = self._reshape_variables(renamed, self.variables) 
+        return out     
+
