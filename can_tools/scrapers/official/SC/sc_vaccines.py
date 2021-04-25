cd8de73c4f26a2198715c433e1f0070f25eee129
Sean McClure <sean.mcclure17@gmail.com>
2021-04-25 19:34:49 -0400

    black

--- can_tools/scrapers/official/SC/sc_vaccines.py
+++ can_tools/scrapers/official/SC/sc_vaccines.py
@@ -4,7 +4,7 @@ from tableauscraper import TableauScraper as TS
 from can_tools.scrapers import variables
 from can_tools.scrapers.official.base import StateDashboard
 
-    
+
 class SCVaccineDemographics(StateDashboard):
     has_location = False
     source = "https://scdhec.gov/covid19/covid-19-vaccination-dashboard"
@@ -12,50 +12,61 @@ class SCVaccineDemographics(StateDashboard):
     state_fips = int(us.states.lookup("South Carolina").fips)
     location_type = "county"
     timezone = "US/Eastern"
-    fullUrl = f'https://public.tableau.com/views/COVIDVaccineDashboard/RECIPIENTVIEW'
-    cmus = {
-        'total_vaccine_initiated': variables.INITIATING_VACCINATIONS_ALL
-    }
+    fullUrl = f"https://public.tableau.com/views/COVIDVaccineDashboard/RECIPIENTVIEW"
+    cmus = {"total_vaccine_initiated": variables.INITIATING_VACCINATIONS_ALL}
 
     def fetch(self) -> TS:
         ts = TS()
         ts.loads(self.fullUrl)
         return ts
 
-    def normalize(self, data:pd.DataFrame) -> pd.DataFrame:
-        
+    def normalize(self, data: pd.DataFrame) -> pd.DataFrame:
+
         # get main sheet
         ws = data.getWorksheet("Vaccine Map By SC residents PEOPLE")
-        counties = ws.getFilters()[0]['values']
+        counties = ws.getFilters()[0]["values"]
         c = pd.DataFrame(counties)
 
         dfs = []
         for county in counties:
             # set county and get subsheet data
             print("working on: ", county)
-            wb = ws.setFilter('Recipient County for maps', county)
+            wb = ws.setFilter("Recipient County for maps", county)
             county_view = wb.getWorksheet("Final Age xSex x Race REC").data
-            county_view['location_name'] = county
+            county_view["location_name"] = county
             dfs.append(county_view)
         df = pd.concat(dfs)
 
         # format data
-        cols = {'Age Bins SIMON1-alias':'age', 'AGG(Count individuals with Suppression  for bar graph)-alias':'value', 'Assigned Race-alias':'race', 'recip sex (Recipient Data Vaccine Dashboard v1.csv)-alias':'sex', 'location_name':'location_name'}
+        cols = {
+            "Age Bins SIMON1-alias": "age",
+            "AGG(Count individuals with Suppression  for bar graph)-alias": "value",
+            "Assigned Race-alias": "race",
+            "recip sex (Recipient Data Vaccine Dashboard v1.csv)-alias": "sex",
+            "location_name": "location_name",
+        }
         df = df[cols.keys()]
         df = (
             df.rename(columns=cols)
-            .replace({'65+':'65_plus', 'M':'male', 'F':'female','Asian/NHPI/AIAN*':'asian'})
+            .replace(
+                {
+                    "65+": "65_plus",
+                    "M": "male",
+                    "F": "female",
+                    "Asian/NHPI/AIAN*": "asian",
+                }
+            )
             .assign(
                 value=lambda x: pd.to_numeric(
                     x["value"].astype(str).str.replace(",", "")
                 ),
-                race=lambda x: x['race'].str.replace('*', '').str.lower(),
-                ethnicity='all',
-                category='total_vaccine_initiated',
-                measurement='cumulative',
-                unit='people',
+                race=lambda x: x["race"].str.replace("*", "").str.lower(),
+                ethnicity="all",
+                category="total_vaccine_initiated",
+                measurement="cumulative",
+                unit="people",
                 vintage=self._retrieve_vintage(),
-                dt=self._retrieve_dtm1d(self.timezone)
+                dt=self._retrieve_dtm1d(self.timezone),
             )
         )
         # i believe -1 is a placeholder for NA
