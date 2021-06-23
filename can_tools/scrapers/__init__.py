6152a90f52a13b986bde4b3f50a2430f121d4b24
Sean McClure <sean.mcclure17@gmail.com>
2021-06-23 18:35:23 -0400

    add class to __init__ file

--- can_tools/scrapers/__init__.py
+++ can_tools/scrapers/__init__.py
@@ -116,6 +116,8 @@ from can_tools.scrapers.official.NE.ne_vaccines import (
     NebraskaVaccineAge,
 )
 
+from can_tools.scrapers.official.NE.ne_cases import NebraskaCases
+
 from can_tools.scrapers.official.NJ.nj_vaccine import NewJerseyVaccineCounty
 from can_tools.scrapers.official.NM.nm_vaccine import NewMexicoVaccineCounty
 from can_tools.scrapers.official.NV.nv_vaccines import NevadaCountyVaccines
