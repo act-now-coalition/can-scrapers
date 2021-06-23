e61c7175bdb7cadc88527b1856861343426c5a2f
Sean McClure <sean.mcclure17@gmail.com>
2021-06-23 09:50:23 -0400

    rollback NE changes on this branch

--- can_tools/scrapers/__init__.py
+++ can_tools/scrapers/__init__.py
@@ -116,8 +116,6 @@ from can_tools.scrapers.official.NE.ne_vaccines import (
     NebraskaVaccineAge,
 )
 
-from can_tools.scrapers.official.NE.ne_cases import NebraskaCases
-
 from can_tools.scrapers.official.NJ.nj_vaccine import NewJerseyVaccineCounty
 from can_tools.scrapers.official.NM.nm_vaccine import NewMexicoVaccineCounty
 from can_tools.scrapers.official.NV.nv_vaccines import NevadaCountyVaccines
