7f9120e5a2698c273b07db07936eedf1040e0df1
Sean McClure <sean.mcclure17@gmail.com>
2021-11-10 10:55:43 -0500

    refactor utils

--- can_tools/utils.py
+++ can_tools/utils.py
@@ -2,6 +2,7 @@ from typing import Optional
 import pandas as pd
 import sqlalchemy as sa
 from sqlalchemy.engine.base import Engine
+from can_tools.models import create_dev_engine
 
 
 def determine_location_column(df: pd.DataFrame) -> str:
@@ -45,23 +46,34 @@ def load_most_recent_cdc(
     pass
 
 
-def find_unknown_variable_id(engine: Engine, df: pd.DataFrame):
+def find_unknown_variable_id(df: pd.DataFrame, engine: Engine = None):
     """Find any CMU variables in the specified dataframe that do not match an entry in the covid_variables file"""
+    if not engine:
+        engine = create_dev_engine()[0]
     variables = pd.read_sql("select * from covid_variables", engine)
     merged = df.merge(variables, on=["category", "measurement", "unit"], how="left")
     bad = merged["id"].isna()
     return df.loc[bad, :]
 
 
-def find_unknown_location_id(engine: Engine, df: pd.DataFrame, state_fips: int):
+def find_unknown_location_id(df: pd.DataFrame, state_fips: int, engine: Engine = None):
     """Find any locations in the specified dataframe that do not match an entry in the locations file"""
+    if not engine:
+        engine = create_dev_engine()[0]
     locs = pd.read_sql("select * from locations", engine)
-    good_rows = df.location_name.isin(locs.loc[locs.state_fips == state_fips, :].name)
+    if "location" in df.columns:
+        good_rows = df.location.isin(locs.location)
+    else:
+        good_rows = df.location_name.isin(
+            locs.loc[locs.state_fips == state_fips, :].name
+        )
     return df.loc[~good_rows, :]
 
 
-def find_unknown_demographic_id(engine: Engine, df: pd.DataFrame):
+def find_unknown_demographic_id(df: pd.DataFrame, engine: Engine = None):
     """Find any demographic pairs in the specified dataframe that do not match an entry in the covid_demographics file"""
+    if not engine:
+        engine = create_dev_engine()[0]
     dems = pd.read_sql("select * from covid_demographics", engine)
     merged = df.merge(dems, on=["sex", "age", "race", "ethnicity"], how="left")
     bad = list(merged["id"].isna())
