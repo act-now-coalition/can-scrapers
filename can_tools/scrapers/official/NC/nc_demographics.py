4ed9416b69500d9313e3341404b922c8d32bdd79
Sean McClure <sean.mcclure17@gmail.com>
2021-11-11 23:41:42 -0500

    add NC

--- can_tools/scrapers/official/NC/nc_demographics.py
+++ can_tools/scrapers/official/NC/nc_demographics.py
@@ -1,94 +1,33 @@
-import pandas as pd
-import us
-import os
-import multiprocessing as mp
 from tableauscraper import TableauScraper
 
 from can_tools.scrapers import variables
 from can_tools.scrapers import NCVaccine
 
+# This uses the tableauscraper module from https://github.com/bertrandmartel/tableau-scraping
+# which "mimics" the actions on a dashboard (like filtering from dropdowns, clicking on maps etc.)
+# The docs are pretty good and there are helpful examples there, but knowing what filters/parameters to
+# use when and in what order can be unclear.
+
+# In general, I usually go to the dashboard with the network inspector open, 
+# navigate to the data we want to pull, and try to match the names of the calls to the TableauScraper filtering functions.
+# E.g. a network call that sends a `set-parameter-value` corresponds to the workbook.GetParameter(...) method,
+# and a `select` call corresponds to the `worksheet.select(...)` function.
+
 
 class NCVaccineAge(NCVaccine):
-    viewPath = "NCDHHS_COVID-19_Dashboard_Vaccinations/VaccinationDashboard"
-    timezone = "US/Eastern"
-    filterFunctionName = "[Parameters].[Parameter 3 1]"  # county
-    # secondaryFilterFunctionName = (
-        # "[Parameters].[Param.DemographicMetric (copy)_1353894643018190853]"  # dose type
-    # )
-    # thirdFilterFunctionName = "[Parameters].[Parameter 2]"  # specify providers
-    # thirdFilterFunctionValue = "3"
-    baseurl = "https://public.tableau.com"
-    viewPath = "NCDHHS_COVID-19_Dashboard_Vaccinations/VaccinationDashboard"
-
     # map wide form column names into CMUs
     variables = {
-        " Population Vaccinated with at Least One Dose": variables.PERCENTAGE_PEOPLE_INITIATING_VACCINE,
-        " Population Fully Vaccinated": variables.PERCENTAGE_PEOPLE_COMPLETING_VACCINE,
-    }
-
-    worksheet = "Age_Percent_Pop_County"
-    demo_col = "age"
-    demo_rename = "Age Group-alias"
-    demo_replacements = {
-        "Missing or Undisclosed": "unknown",
-        "75+": "75_plus",
-        "0-17 (16-17)": "0-17",
+        "": variables.PERCENTAGE_PEOPLE_INITIATING_VACCINE,
+        "": variables.PERCENTAGE_PEOPLE_COMPLETING_VACCINE,
     }
 
     def fetch(self):
-        # engine = TableauScraper()
-        # engine.loads("https://public.tableau.com/views/NCDHHS_COVID-19_Dashboard_Vaccinations/VaccinationDashboard")
-        # engine = engine.getWorkbook()
-        # # print(engine.getParameters())
-        # sp = engine.goToStoryPoint("2")
-        # print(sp.getParameters())
-        # wb = sp.setParameter("County", "Wake County")
-        # return wb
+        engine = TableauScraper()
+        engine.loads("https://public.tableau.com/views/NCDHHS_COVID-19_Dashboard_Vaccinations/VaccinationDashboard")
+        engine = engine.getWorkbook()
+        sp = engine.goToStoryPoint("2")
+        wb = sp.setParameter("County", "Wake County")
 
-        # g = wb.getWorksheet("Race_weekly_county")
-        # return g
-        # print(book.getStoryPoints())
-        # print("go to specific storypoint")
-
-        # book.setParameter("County-Map", "Wake County")
-        # story = book.goToStoryPoint(storyPointId=2)
-        # return book
-
-        counties = self._retrieve_counties()
-        return self._get_data(counties)
-
-    def _get_data(self, counties):
-        data = []
-        for county in [counties[0]]:
-            self.filterFunctionValue = county + " County"
-            # for dose_val in ["3", "4"]:
-                # print("wor/king on:", county, "dose: ", dose_val)
-                # self.secondaryFilterValue = dose_val
-            df = self.get_tableau_view(
-                url="https://public.tableau.com/views/NCDHHS_COVID-19_Dashboard_Vaccinations/VaccinationDashboard"
-            )
-            return df
-            # [self.worksheet]
-            data.append(df.assign(location_name=county))
-        return data
-
-    def normalize(self, data: pd.DataFrame) -> pd.DataFrame:
-        df = (
-            data.rename(
-                columns={
-                    self.demo_rename: self.demo_col,
-                    "AGG(calc.RunningSum.DemographicMetric)-alias": "value",
-                    "ATTR(text.tooltips)-alias": "variable",
-                    "Week of-value": "dt",
-                }
-            )
-            .loc[:, ["location_name", "dt", "variable", "value", self.demo_col]]
-            .query(f"variable != '%missing%' and {self.demo_col} != 'Suppressed'")
-            .replace(self.demo_replacements)
-            .assign(
-                value=lambda x: pd.to_numeric(x["value"].astype(str)) * 100,
-                vintage=self._retrieve_vintage(),
-            )
-            .pipe(self.extract_CMU, self.variables, skip_columns=[self.demo_col])
-        )
-        return df.drop(columns={"variable"})
+        # i feel like with the set paramater that this should be returning data in the Race_county... tables
+        # but nothing :/ 
+        return wb.worksheets
