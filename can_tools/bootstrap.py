3a7a92689a2066933a452823d76a928af3c18eb9
Sean McClure <sean.mcclure17@gmail.com>
2021-11-16 23:20:11 -0500

    format

--- can_tools/bootstrap.py
+++ can_tools/bootstrap.py
@@ -3,9 +3,9 @@ import sqlalchemy.orm as sa_orm
 import sqlalchemy as sa
 from can_tools.models import bootstrap
 
-db_url = os.environ.get('CAN_SCRAPERS_DB_URL')
+db_url = os.environ.get("CAN_SCRAPERS_DB_URL")
 if not db_url:
-    raise RuntimeError('Missing CAN_SCRAPERS_DB_URL environment variable.')
+    raise RuntimeError("Missing CAN_SCRAPERS_DB_URL environment variable.")
 
 engine = sa.create_engine(db_url, echo=False)
 Session = sa_orm.sessionmaker(bind=engine)
