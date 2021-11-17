b9bc8bb8dc72ca5c2c1657a1c6f8ee76792dd93c
Sean McClure <sean.mcclure17@gmail.com>
2021-11-17 11:04:41 -0500

    add pip installation and paths

--- services/webhook/update_flows.sh
+++ services/webhook/update_flows.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 git pull origin main
-/home/sglyon/miniconda/envs/prefect-can-scrapers/bin/pip install -e .
+sudo apt install python3-pip
+/home/sglyon/miniconda/envs/prefect-can-scrapers/bin/pip3 install -e /home/sglyon/can-scrapers
 /home/sglyon/miniconda/envs/prefect-can-scrapers/bin/python /home/sglyon/can-scrapers/services/prefect/flows/generated_flows.py
