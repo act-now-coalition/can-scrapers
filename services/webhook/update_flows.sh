76910785ef1a437902c02a7cfb5a510f423e47a4
Sean McClure <sean.mcclure17@gmail.com>
2021-11-17 11:19:53 -0500

    remove pip installation and pip3 -> pip

--- services/webhook/update_flows.sh
+++ services/webhook/update_flows.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 git pull origin main
-sudo apt install python3-pip
-/home/sglyon/miniconda/envs/prefect-can-scrapers/bin/pip3 install -e /home/sglyon/can-scrapers
+/home/sglyon/miniconda/envs/prefect-can-scrapers/bin/pip install -e /home/sglyon/can-scrapers
 /home/sglyon/miniconda/envs/prefect-can-scrapers/bin/python /home/sglyon/can-scrapers/services/prefect/flows/generated_flows.py
