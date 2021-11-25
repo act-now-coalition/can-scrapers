c17cba6c1dba5ad25eb56ed1d475c2da74538d22
Christopher Lin <powertothepenguins@gmail.com>
2021-11-25 12:40:39 -0800

    clean up

--- generate_parquet_files.py
+++ generate_parquet_files.py
@@ -1,16 +1,15 @@
-import logging
+import os
+import pandas as pd
+import pathlib
+import prefect
 import sqlalchemy as sa
-import sys
 
-from contextlib import closing
-from prefect import Flow, Parameter, task, unmapped
+from prefect import Flow, task, unmapped
 from prefect.tasks.secrets import EnvVarSecret
 
-#logging.basicConfig(
-#        stream=sys.stdout,
-#        level=logging.INFO)
-        #format='[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s')
-        #format='[%(asctime)s] %(levelname)s - %(message)s')
+DATA_PATH = pathlib.Path(os.environ["DATAPATH"]) / "final"
+DATA_PATH.mkdir(parents=True, exist_ok=True)
+FILENAME_PREFIX = "can_scrape_api_covid_us"
 
 @task
 def fetch_location_ids(connstr: str):
@@ -21,17 +20,28 @@ def fetch_location_ids(connstr: str):
         return location_ids
 
 @task
-def process_location(connstr: str, location_id: str):
+def create_location_parquet(connstr: str, location_id: str):
     engine = sa.create_engine(connstr)
     with engine.connect() as conn:
-        rows = conn.execute(sa.text('SELECT * FROM data.covid_observations WHERE location_id = :location_id'), {'location_id': location_id}).fetchall()
-    print(rows)
+        # Read rows from PostgreSQL into Pandas dataframe.
+        query = sa.text('SELECT * FROM data.covid_observations WHERE location_id = :location_id').bindparams(location_id=location_id)
+        df = pd.read_sql_query(query, conn)
+
+        # Write vintage file.
+        ts = prefect.context.scheduled_start_time
+        dt_str = pd.to_datetime(ts).strftime("%Y-%m-%dT%H")
+        vintage_fn = f'{FILENAME_PREFIX}_{dt_str}_{location_id}.parquet'
+        df.to_parquet(DATA_PATH / vintage_fn, index=False)
+
+        # Replace primary file.
+        fn = f'{FILENAME_PREFIX}_{location_id}.parquet'
+        df.to_parquet(DATA_PATH / fn, index=False)
 
 def main():
-    with Flow('update_parquet_files') as flow:
+    with Flow('update_location_parquet_files') as flow:
         connstr = EnvVarSecret("COVID_DB_CONN_URI")
         location_ids = fetch_location_ids(connstr)
-        process_location.map(connstr=unmapped(connstr), location_id=location_ids)
+        create_location_parquet.map(connstr=unmapped(connstr), location_id=location_ids)
 
     flow.run()
 
