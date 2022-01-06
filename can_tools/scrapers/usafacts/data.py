f5b5e000b650ecf2f87dd5a38a1dcdc3da365ab3
sean mcclure <55333380+smcclure17@users.noreply.github.com>
2022-01-06 12:32:07 -0500

    Remove date filters from USAFacts scrapers (#393)
    * remove date filters
    
    * formatting

--- can_tools/scrapers/usafacts/data.py
+++ can_tools/scrapers/usafacts/data.py
@@ -44,17 +44,8 @@ class USAFactsBase(FederalDashboard):
         # Make lowercase so they can't change capitalization on us
         data.columns = [c.lower() for c in data.columns]
 
-        # Load data from site and move dates from column names to
-        # a new variable
-        cols = ["countyfips", "county name", "state", "statefips"]
-        cols = cols + [c for c in data.columns if ("2020" in c) or ("2021" in c)]
-
-        df = (
-            data.loc[:, cols]
-            .drop(["county name", "state"], axis=1)
-            .melt(
-                id_vars=["countyfips", "statefips"], var_name="dt", value_name="value"
-            )
+        df = data.drop(["county name", "state"], axis=1).melt(
+            id_vars=["countyfips", "statefips"], var_name="dt", value_name="value"
         )
         df["dt"] = pd.to_datetime(df["dt"])
 
