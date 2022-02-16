0f7bb721c407559a70aed936edb7cc128e4beb92
Sean McClure <sean.mcclure17@gmail.com>
2022-02-16 13:31:14 -0500

    Add NYT and Parquet updater flow

---
+++ services/prefect/flows/nyt_updater.py
@@ -0,0 +1,26 @@
+from prefect import Flow
+from prefect.schedules import CronSchedule
+
+from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
+
+
+def init_updater_flow():
+    with Flow("NYTParquetUpdater", schedule=CronSchedule("30 6 * * *")) as flow:
+
+        # NYTimesCasesDeaths and UpdateParquetFiles flows must already be registered via
+        # generated_flows.py and update_api_view.py
+        nyt_flow = create_flow_run(
+            flow_name="NYTimesCasesDeaths", project_name="can-scrape"
+        )
+        wait_for_nyt = wait_for_flow_run(nyt_flow, raise_final_state=True)
+
+        flow_b = create_flow_run(
+            flow_name="UpdateParquetFiles", project_name="can-scrape"
+        )
+        flow_b.set_upstream(wait_for_nyt)
+
+    flow.register(project_name="can-scrape")
+
+
+if __name__ == "__main__":
+    init_updater_flow()
