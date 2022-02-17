6d12f1125ab8c7bebf3ee68ef7ced8aad215d819
Sean McClure <sean.mcclure17@gmail.com>
2022-02-17 13:02:28 -0500

    Reformat task execution for clarity

--- services/prefect/flows/scheduled_nyt_and_parquet_updater.py
+++ services/prefect/flows/scheduled_nyt_and_parquet_updater.py
@@ -1,40 +1,53 @@
 from prefect import Flow
 from prefect.schedules import CronSchedule
-
 from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
 
 
-def init_nyt_scheduled_flow():
+def init_scheduled_nyt_updater_flow():
     """Flow to check the NYT source for new data, ingest the data, and update the parquet file.
 
     Flow runs every 30 minutes. If no new data is detected then UpdateParquetFiles is skipped.
     If new data is detected, UpdateParquetFiles is executed.
 
     """
-
-    with Flow("NYTParquetUpdater", schedule=CronSchedule("*/30 * * * *")) as flow:
+    with Flow(
+        "NYTParquetScheduledUpdater", schedule=CronSchedule("*/30 * * * *")
+    ) as parent_flow:
 
         # Note that the below code relies on NYTimesCasesDeaths and UpdateParquetFiles flows
         # already being registered via generated_flows.py and update_api_view.py.
         # If the MainFlow flow is running, this generally means that these flows have been registered.
         nyt_flow = create_flow_run(
-            flow_name="NYTimesCasesDeaths", project_name="can-scrape"
+            flow_name="NYTimesCasesDeaths",
+            project_name="can-scrape",
+            task_args=dict(name="Create NYTimesCasesDeaths flow"),
         )
-        wait_for_nyt = wait_for_flow_run(nyt_flow, raise_final_state=True)
+        wait_for_nyt = wait_for_flow_run(
+            nyt_flow,
+            raise_final_state=True,
+            stream_logs=True,
+            task_args=dict(name="run NYTimesCasesDeaths flow"),
+        )
 
+        # Execute on success, skip on skipped result of wait_for_nyt
         parquet_flow = create_flow_run(
-            flow_name="UpdateParquetFiles", project_name="can-scrape"
+            flow_name="UpdateParquetFiles",
+            project_name="can-scrape",
+            upstream_tasks=[wait_for_nyt],
+            task_args=dict(
+                name="Create UpdateParquetFiles flow", skip_on_upstream_skip=True
+            ),
+        )
+        # Wait for this flow to succeed/fail before setting the final state of parent_flow
+        run_parquet_flow = wait_for_flow_run(
+            parquet_flow,
+            raise_final_state=True,
+            stream_logs=True,
+            task_args=dict(name="Run UpdateParquetFiles flow"),
         )
 
-        # Wait for the parquet flow to finish before setting parent flow state to success.
-        finish_parquet_flow = wait_for_flow_run(parquet_flow, raise_final_state=True)
-
-        # flow will only trigger if wait_for_nyt finishes with Success state.
-        # so if the NYT scraper is skipped, the parquet flow will not execute.
-        parquet_flow.set_upstream(wait_for_nyt)
-
-    flow.register(project_name="can-scrape")
+    parent_flow.register("can-scrape")
 
 
 if __name__ == "__main__":
-    init_nyt_scheduled_flow()
+    init_scheduled_nyt_updater_flow()
