36e4e398278a974e916a1b28b6bae2f460d96a79
sean mcclure <55333380+smcclure17@users.noreply.github.com>
2022-02-20 15:49:37 -0500

    Trigger covid-data-model Update Combined Datasets action from Prefect flow (#413)
    * Add NYT and Parquet updater flow
    
    * Rename flow
    
    * Add wait_for_parquet
    
    * Add covid-data-model workflow trigger
    
    * stash
    
    * Update flow logic to skip on etag skip
    
    * Reformat task execution for clarity
    
    * Use custom skip flag instead of message
    
    * Black formatting
    
    * Clarify docstring
    
    * Clarify etag_cache_flag use
    
    * Fix request, formatting
    
    * Delete old file
    
    * Add slack notifier

--- services/prefect/flows/scheduled_nyt_and_parquet_updater.py
+++ services/prefect/flows/scheduled_nyt_and_parquet_updater.py
@@ -1,17 +1,52 @@
-from prefect import Flow
+import requests
+from datetime import timedelta
+from prefect import Flow, task
+from prefect.engine.state import Failed
 from prefect.schedules import CronSchedule
+from prefect.tasks.secrets import PrefectSecret
+from prefect.utilities.notifications import slack_notifier
 from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
+
+
+@task(max_retries=3, retry_delay=timedelta(minutes=1))
+def make_request(github_token):
+    dispatch_url = (
+        "https://api.github.com/repos/covid-projections/covid-data-model/"
+        "actions/workflows/update_repo_datasets.yml/dispatches"
+    )
+    response = requests.post(
+        url=dispatch_url,
+        headers={
+            "Authorization": f"token {github_token}",
+            "Accept": "application/vnd.github.v3+json",
+        },
+        data='{"ref": "main"}',
+    )
+    response.raise_for_status()  # raise status in case of failure
+
+
+def init_update_datasets_flow():
+    """Initialize a flow to kick off the covid-data-model Update Combined Datasets Github action"""
+    with Flow("TriggerUpdateCombinedDatasets") as flow:
+        github_token = PrefectSecret("GITHUB_ACTION_PAT")
+        make_request(github_token=github_token)
+    flow.register("can-scrape")
 
 
 def init_scheduled_nyt_updater_flow():
     """Flow to check the NYT source for new data, ingest the data, and update the parquet file.
 
-    Flow runs every 30 minutes. If no new data is detected then UpdateParquetFiles is skipped.
+    Flow runs every 30 minutes. If no new data is detected then UpdateParquetFiles and
+    TriggerUpdateCombinedDatasets are skipped.
+    
     This is the main updater responsible for updating and ingesting the NYT cases and deaths data.
-
     """
     with Flow(
-        "NYTParquetScheduledUpdater", schedule=CronSchedule("*/30 * * * *")
+        "NYTParquetScheduledUpdater",
+        schedule=CronSchedule("*/30 * * * *"),
+        state_handlers=[
+            slack_notifier(only_states=[Failed])
+        ],  # only alert us on failure
     ) as parent_flow:
 
         # Note that the below code relies on NYTimesCasesDeaths and UpdateParquetFiles flows
@@ -38,16 +73,34 @@ def init_scheduled_nyt_updater_flow():
                 name="Create UpdateParquetFiles flow", skip_on_upstream_skip=True
             ),
         )
-        # Wait for this flow to succeed/fail before setting the final state of parent_flow
-        run_parquet_flow = wait_for_flow_run(
+        wait_for_parquet_flow = wait_for_flow_run(
             parquet_flow,
             raise_final_state=True,
             stream_logs=True,
             task_args=dict(name="Run UpdateParquetFiles flow"),
         )
 
+        trigger_update_datasets_flow = create_flow_run(
+            flow_name="TriggerUpdateCombinedDatasets",
+            project_name="can-scrape",
+            upstream_tasks=[wait_for_parquet_flow],
+            task_args=dict(
+                name="Create TriggerUpdateCombinedDatasets flow",
+                skip_on_upstream_skip=True,
+            ),
+        )
+
+        # Wait for this flow to succeed/fail before setting the final state of parent_flow
+        run_trigger_update_datasets_flow = wait_for_flow_run(
+            trigger_update_datasets_flow,
+            raise_final_state=True,
+            stream_logs=True,
+            task_args=dict(name="Run TriggerUpdateCombinedDatasets flow"),
+        )
+
     parent_flow.register("can-scrape")
 
 
 if __name__ == "__main__":
+    init_update_datasets_flow()
     init_scheduled_nyt_updater_flow()
